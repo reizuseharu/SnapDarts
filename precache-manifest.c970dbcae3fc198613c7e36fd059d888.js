c301b72cdb8617fc0f63223b1f87eea4540dc9b6
Reizu Seharu <reizu.seharu@gmail.com>
2020-04-10 02:02:33 -0400

    Updates

---
+++ precache-manifest.c970dbcae3fc198613c7e36fd059d888.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc021a6f49ad3cba1c35b83714fd792",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec001eee9168853b6805",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "7d204e61cfacddfd1c21",
+    "url": "/static/css/main.ecfe934d.chunk.css"
+  },
+  {
+    "revision": "ec001eee9168853b6805",
+    "url": "/static/js/2.27286942.chunk.js"
+  },
+  {
+    "revision": "c0591b1c3357938035d00c845cddc8a2",
+    "url": "/static/js/2.27286942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d204e61cfacddfd1c21",
+    "url": "/static/js/main.857470cd.chunk.js"
+  },
+  {
+    "revision": "714035ffef50f90b41c9",
+    "url": "/static/js/runtime-main.696b4ca4.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
