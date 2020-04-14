012072d9432ec799444c8c1e0d1fe895316e75d8
Reizu Seharu <reizu.seharu@gmail.com>
2020-04-14 01:23:14 -0400

    Updates

---
+++ Main.js
@@ -0,0 +1,9 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+function createWindow () {
+  // Create the browser window.
+    let win = new BrowserWindow({width: 800, height: 600});
+    win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
+}
+app.on('ready', createWindow);
---
+++ precache-manifest.526c93c0c177b5b972ddead80da84420.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa5748f3191978fb12b83404ba3cf01",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5749bc710d17c74d8ccb",
+    "url": "./static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "a07f5f99380338d6d267",
+    "url": "./static/css/main.b7813d11.chunk.css"
+  },
+  {
+    "revision": "5749bc710d17c74d8ccb",
+    "url": "./static/js/2.539f54c8.chunk.js"
+  },
+  {
+    "revision": "ba9adbbacafce83be8be9e61322755e5",
+    "url": "./static/js/2.539f54c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07f5f99380338d6d267",
+    "url": "./static/js/main.52c88a8d.chunk.js"
+  },
+  {
+    "revision": "398d8f5b1f699ad9713b",
+    "url": "./static/js/runtime-main.0ae2b862.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "./static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "./static/media/sidebar-2.310509c9.jpg"
+  }
+]);
